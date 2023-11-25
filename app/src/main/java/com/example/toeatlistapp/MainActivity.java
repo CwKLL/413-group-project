c30cd5976655ce9a209a5f954fc35467afa75e23
CwKLL <cwkll5670@gmail.com>
2023-11-25 17:00:04 +0800

    1.5

--- app/src/main/java/com/example/toeatlistapp/MainActivity.java
+++ app/src/main/java/com/example/toeatlistapp/MainActivity.java
@@ -1,14 +1,24 @@
 package com.example.toeatlistapp;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.android.material.navigation.NavigationView;
+
 public class MainActivity extends AppCompatActivity {
     Button addRestaurantButton;
+    private DrawerLayout drawerLayout;
+    private NavigationView navigationView;
+    private ActionBarDrawerToggle toggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +26,31 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         addRestaurantButton = findViewById(R.id.addRestaurantButton);
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.nav_view);
+
+        toggle = new ActionBarDrawerToggle(
+                this, drawerLayout, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                if (item.getItemId() == navigationView.getMenu().findItem(R.id.nav_view_restaurants).getItemId()) {
+                    Intent intent = new Intent(MainActivity.this, ViewRestaurantsActivity.class);
+                    startActivity(intent);
+                }
+
+                // Add this line to uncheck the item after it's clicked
+                item.setChecked(false);
+
+                drawerLayout.closeDrawer(GravityCompat.START);
+                return true;
+            }
+        });
 
         addRestaurantButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -24,14 +59,13 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
 
-        Button viewRestaurantsButton = findViewById(R.id.view_restaurants_button);
-        viewRestaurantsButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, ViewRestaurantsActivity.class);
-                startActivity(intent);
-            }
-        });
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (toggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
